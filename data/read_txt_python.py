df4cddac9e869fdc59b7173ba95f5bc444e9d712
PascalChristiaanse <pascal.christiaanse@gmail.com>
2024-12-24 19:06:42 +0100

    add datastructure

--- data/read_txt_python.py
+++ data/read_txt_python.py
@@ -1,24 +1,43 @@
 import numpy as np
+import pandas as pd
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-t = np.loadtxt('t.txt') # epochs - s
 
-CA_range = np.loadtxt('CA_range.txt') # pseudorange observations from CA code - km
 
-PRN_ID = np.loadtxt('PRN_ID.txt') # PRN ID of tracked GPS satellites
+class Data:
+    def __init__(self):
+        self.t = np.loadtxt('data/t.txt') # epochs - s
 
-rx_gps = np.loadtxt('rx_gps.txt') # GPS satellite positions (transmitters) - km
-ry_gps = np.loadtxt('ry_gps.txt')
-rz_gps = np.loadtxt('rz_gps.txt')
+        self.CA_range = np.loadtxt('data/CA_range.txt') # pseudorange observations from CA code - km
 
-vx_gps = np.loadtxt('vx_gps.txt') # GPS satellite velocities (transmitters) - km/s
-vy_gps = np.loadtxt('vy_gps.txt')
-vz_gps = np.loadtxt('vz_gps.txt')
+        self.PRN_ID = np.loadtxt('data/PRN_ID.txt') # PRN ID of tracked GPS satellites
 
-rx = np.loadtxt('rx.txt') # precise positions (receivers) - km
-ry = np.loadtxt('ry.txt')
-rz = np.loadtxt('rz.txt')
+        self.clk_gps = np.loadtxt('data/clk_gps.txt') # clock correction for GPS sats (transmitters) - s
 
-vx = np.loadtxt('vx.txt') # precise velocities (receivers) - km/s
-vy = np.loadtxt('vy.txt')
-vz = np.loadtxt('vz.txt')
+        self.rx_gps = np.loadtxt('data/rx_gps.txt') # GPS satellite positions (transmitters) - km
+        self.ry_gps = np.loadtxt('data/ry_gps.txt')
+        self.rz_gps = np.loadtxt('data/rz_gps.txt')
 
+        self.vx_gps = np.loadtxt('data/vx_gps.txt') # GPS satellite velocities (transmitters) - km/s
+        self.vy_gps = np.loadtxt('data/vy_gps.txt')
+        self.vz_gps = np.loadtxt('data/vz_gps.txt')
+
+        self.rx = np.loadtxt('data/rx.txt') # precise positions (receivers) - km
+        self.ry = np.loadtxt('data/ry.txt')
+        self.rz = np.loadtxt('data/rz.txt')
+
+        self.vx = np.loadtxt('data/vx.txt') # precise velocities (receivers) - km/s
+        self.vy = np.loadtxt('data/vy.txt')
+        self.vz = np.loadtxt('data/vz.txt')
+
+        self.c = 299792.458     # Speed of light [ms^-1]
+        self.omg_e = 7.292115e-5 # rotational velocity of earth [radssec^-1]
+    
+    
+def main():
+    data = Data()  
+        
+if __name__ == "__main__":
+    main()
